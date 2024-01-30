847f2106425bef5134613eaafeae62f1a623256c
NarendraVarma87 <narendravarma97047@gmail.com>
2024-01-30 18:18:55 +0530

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,41 @@
-import React from 'react' 
-import { useSelector } from 'react-redux'
-import './Style.css'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './Style.css';
 
 export default function Navbar() {
-  const cartCount  = useSelector((state) => state.cartReducer.cartValue.length)
-  const price = useSelector((state) => state.cartReducer.totalPrice)
+  const cartCount = useSelector((state) => state.cartReducer.cartValue.length);
+  const price = useSelector((state) => state.cartReducer.totalPrice);
+
   return (
-    <div className='navbar container m-5'>
-    <nav className="navbar navbar-expand-md  fixed-top mx-5" id='first'>
-    <a href="#" className="navbar-brand h1"> Mobile Store</a>
-    <button type="button" data-bs-toggle ="collapse" data-bs-target='#collapsenavbar'
-    className="navbar-toggler" aria-aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigator'>
-    <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="collapsenavbar">
-        <ul className='navbar-nav mx-auto'>
-            <li className="nav-item text-center mx-5"><a href='#' className="nav-link active btn btn-primary">Cart Items : {cartCount}</a></li>
-            <li className="nav-item text-center mx-5"><a href='#' className="nav-link active btn btn-primary">Total Price : {price}</a></li>
+    <nav className="navbar navbar-expand-md fixed-top mx-5" id="first">
+      <a href="#" className="navbar-brand h1">
+        Mobile Store
+      </a>
+      <button
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#collapsenavbar"
+        className="navbar-toggler"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigator"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="collapsenavbar">
+        <ul className="navbar-nav mx-auto">
+          <li className="nav-item text-center mx-5">
+            <a href="#" className="nav-link active btn btn-primary">
+              Cart Items : {cartCount}
+            </a>
+          </li>
+          <li className="nav-item text-center mx-5">
+            <a href="#" className="nav-link active btn btn-primary">
+              Total Price : {price}
+            </a>
+          </li>
         </ul>
-        </div>
-        </nav>
-        </div>
-  )
+      </div>
+    </nav>
+  );
 }
